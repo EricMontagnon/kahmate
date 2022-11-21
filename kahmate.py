bf408d3dd05799d7ce1c81a788f1287ee9c73d7e
catarinaerickson <catarinaerickson22@gmail.com>
2022-11-21 15:31:38 +0100

    creation of venv

--- kahmate.py
+++ kahmate.py
@@ -1,17 +1,6 @@
 import model
 import random
 
-UNICODE_COLORS = {
-    None: "\U00002B1A",
-    model.Color.BLACK: "\U00002B1B",
-    model.Color.BLUE: "\U0001F7E6",
-    model.Color.GREEN: "\U0001F7E9",
-    model.Color.RED: "\U0001F7E5",
-    model.Color.WHITE: "\U00002B1C",
-    model.Color.YELLOW: "\U0001F7E8",
-}
-
-
 class Game:
     """
     The state of the game, defined by:
--- tests.py
+++ tests.py
@@ -2,17 +2,17 @@ import model
 import kahmate
 
 def test_initialisation_piece():
-    piece1 = model.Piece(1, 1, [100, 100], False, False)
+    piece1 = model.Piece(model.PieceType.BIG, [100, 100], False, False)
     assert piece1.position() == [100, 100]
 
 def test_initialisation_player():
-    piece1 = model.Piece(1, 1, [100, 100], False, False)
+    piece1 = model.Piece(model.PieceType.BIG, [100, 100], False, False)
     player1 = model.HumanPlayer("Jacques", [piece1], "blue")
     assert piece1.position() == [100, 100]
 
 def test_initialisation_jeu():
-    piece1 = model.Piece(1, 1, [100, 100], False, False)
-    piece2 = model.Piece(1, 1, [500, 500], False, False)
+    piece1 = model.Piece(model.PieceType.BIG, [100, 100], False, False)
+    piece2 = model.Piece(model.PieceType.BIG, [500, 500], False, False)
     game = kahmate.Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"])
     positions = game.get_positions()
     assert positions == [[[100, 100]], [[500, 500]]]
