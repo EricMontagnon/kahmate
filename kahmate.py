d822ddf44391793ef99963f55541b83809e9a043
EricMontagnon <eric.montagnon@eleves.enpc.fr>
2022-11-26 18:04:46 +0100

    Valid Displacement generation and face off

--- kahmate.py
+++ kahmate.py
@@ -1,58 +1,5 @@
 import model
 import random
 
-class Game:
-    """
-    The state of the game, defined by:
-    - a board;
-    - a list of players
-    - an integer used as the index of the next player in the list of players.
 
-    When building the list of players, the argument `player` is used and:
-    - if an item is a string (`str`) then a human player is built using the
-      string as their name;
-    - if an item is a level (`Level`) then an AI player is built using the
-      level.
-    """
 
-    def __init__(self, players, pieces, colors):
-        self._players = []
-        k = -1
-        for player in players:
-            k += 1
-            if isinstance(player, str):
-                self._players.append(model.HumanPlayer(player, pieces[k], colors[k]))
-            elif isinstance(player, model.Level):
-                self._players.append(model.AIPlayer(player, pieces[k], colors[k]))
-            else:
-                assert False, "unknown player definition"
-        self._currentPlayer = 0
-
-    def __str__(self):
-        ans = "Description of the first team :"
-        ans += "\n" + str(self._players[0])
-        ans += "\n" + " "
-        ans += "\n" + "Description of the second team :"
-        ans += "\n" + str(self._players[1])
-        return ans
-
-    def currentPlayer(self):
-        return self._currentPlayer
-
-    def get_positions(self):
-        return[self._players[0].get_positions(), self._players[1].get_positions()]
-
-    def update_positions(self, playerID, pieceID, new_position, ball):
-        self._players[playerID].update_positions(pieceID, new_position, ball)
-
-    def generate_valid_moves(self):
-        moves = []
-        pass
-
-    def winners(self):
-        pass
-
-piece1 = model.Piece(model.PieceType.BIG, [100, 100], False, False)
-piece2 = model.Piece(model.PieceType.BIG, [500, 500], False, False)
-game = Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"])
-print(game)
--- tests.py
+++ tests.py
@@ -1,31 +1,78 @@
 import model
 import kahmate
 
+i_limit = 600
+j_limit = 800
+
 
 def test_initialisation_piece():
-    piece1 = model.Piece(model.PieceType.BIG, [100, 100], False, False)
-    assert piece1.get_position() == [100, 100]
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    assert str(piece1) == "The big guy located at [100, 100] does not have the ball and is ready to play"
 
 
 def test_initialisation_player():
-    piece1 = model.Piece(model.PieceType.BIG, [100, 100], False, False)
-    player1 = model.HumanPlayer("Jacques", [piece1], "blue")
-    assert piece1.get_position() == [100, 100]
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    player1 = model.HumanPlayer("Jacques", 0, [piece1], "blue")
+    ans = "This is Jacques's Team : \n"
+    ans += "The big guy located at [100, 100] does not have the ball and is ready to play"
+    assert str(player1) == ans
 
 
 def test_initialisation_jeu():
-    piece1 = model.Piece(model.PieceType.BIG, [100, 100], False, False)
-    piece2 = model.Piece(model.PieceType.BIG, [500, 500], False, False)
-    game = kahmate.Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"])
-    positions = game.get_positions()
-    assert positions == [[[100, 100]], [[500, 500]]]
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    piece2 = model.Piece(1, 0, model.PieceType.BIG.value, [500, 500], False, False)
+    game = model.Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"], [int(i_limit/2), int(j_limit/2)])
+    ans = "Description of the first team :\n"
+    ans += "This is Jean's Team : \n"
+    ans += "The big guy located at [100, 100] does not have the ball and is ready to play\n"
+    ans += " \n"
+    ans += "Description of the second team :\n"
+    ans += "This is Jacques's Team : \n"
+    ans += "The big guy located at [500, 500] does not have the ball and is ready to play"
+    assert str(game) == ans
 
 
-def test_movement_initialisation():
-    pass
+def test_set_position_piece():
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    piece1.set_position([200, 200], True, False)
+    assert str(piece1) == "The big guy located at [200, 200] has the ball and is ready to play"
 
 
-def test_basic_movement():
-    pass
+def test_set_position_player():
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    player1 = model.HumanPlayer("Jacques", 0,  [piece1], "blue")
+    player1.update_positions(0, [200, 200], True, False)
+    assert str(piece1) == "The big guy located at [200, 200] has the ball and is ready to play"
 
 
+def test_set_position_game():
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    piece2 = model.Piece(1, 0, model.PieceType.BIG.value, [500, 500], False, False)
+    game = model.Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"], [int(i_limit/2), int(j_limit/2)])
+    game.update_positions(0, 0, [200, 200], True, False)
+    assert str(piece1) == "The big guy located at [200, 200] has the ball and is ready to play"
+
+
+def test_initialisation_displacement():
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    piece2 = model.Piece(1, 0, model.PieceType.BIG.value, [500, 500], False, False)
+    game = model.Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"], [int(i_limit/2), int(j_limit/2)])
+    displacement = game.generate_displacement(0, 0)
+    assert str(displacement[0]) == "Displacement of the piece 0 to the position : [98, 100]"
+
+
+def test_play_displacement():
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    piece2 = model.Piece(1, 0, model.PieceType.BIG.value, [500, 500], False, False)
+    game = model.Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"], [int(i_limit/2), int(j_limit/2)])
+    displacement = game.generate_displacement(0, 0)
+    displacement[0].play(game)
+    assert str(piece1) == "The big guy located at [98, 100] does not have the ball and is ready to play"
+
+
+def test_face_off():
+    piece1 = model.Piece(0, 0, model.PieceType.BIG.value, [100, 100], False, False)
+    piece2 = model.Piece(1, 0, model.PieceType.BIG.value, [500, 500], False, False)
+    game = model.Game(["Jean", "Jacques"], [[piece1], [piece2]], ["blue", "blue"], [int(i_limit/2), int(j_limit/2)])
+    face_off = game.face_off(0, 0, 0)
+    assert face_off == "Attack wins!" or face_off == "Defense wins!"
