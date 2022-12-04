88d271800bd1525fe0b48397be4afb8d52549320
EricMontagnon <eric.montagnon@eleves.enpc.fr>
2022-12-04 13:39:30 +0100

    cleaning + displacements implementation

--- kahmate/model.py
+++ kahmate/model.py
@@ -23,6 +23,9 @@ class PieceType(enum.Enum):
     FAST = [4, -1, 1, 'fast']
     SMART = [3, 0, 1, 'smart']
 
+    def __getitem__(self, index):
+        return self.value[index]
+
 
 class Piece:
     """
@@ -34,9 +37,9 @@ class Piece:
     """
     def __init__(self,  piece_type: PieceType):
         self._piece_type = piece_type
-        self._position = []
-        self._has_ball = False
-        self._is_down = False
+        self.position = []
+        self.has_ball = False
+        self.is_down = False
 
     @ property
     def speed(self):
@@ -58,22 +61,9 @@ class Piece:
     def piece_type(self):
         return self._piece_type
 
-    @property
-    def has_ball(self):
-        return self._has_ball
-
-    @property
-    def is_down(self):
-        return self._is_down
-
-    @property
-    def get_position(self):
-        return self._position
-
-    def set_position(self, position: [int, int], has_ball: bool, down: bool):
-        self._position = position
-        self._has_ball = has_ball
-        self._is_down = down
+    def __str__(self):
+        ans = "The " + self.piece_type.name + " piece is located at " + str(self.position)
+        return ans
 
 
 class Player:
@@ -85,33 +75,29 @@ class Player:
     """
 
     def __init__(self, color):
-        self._pieces = [Piece(piece_type) for piece_type in PieceType]
-        self._pieces.append(Piece(PieceType.REGULAR))
+        self.pieces = [Piece(piece_type) for piece_type in PieceType]
+        self.pieces.append(Piece(PieceType.REGULAR))
         self._color = color
         self._strength_deck = [i for i in range(1, 6)]
-
-    @property
-    def pieces(self):
-        return self._pieces
 
     @property
     def color(self):
         return self._color
 
     def draw(self, screen):
-        for piece in self._pieces:
+        for piece in self.pieces:
             if piece.piece_type == PieceType.REGULAR:
-                screen.blit(self._color.value['REGULAR'], (piece.get_position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.get_position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
+                screen.blit(self._color.value['REGULAR'], (piece.position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
             elif piece.piece_type == PieceType.BIG:
-                screen.blit(self._color.value['BIG'], (piece.get_position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.get_position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
+                screen.blit(self._color.value['BIG'], (piece.position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
             elif piece.piece_type == PieceType.FAST:
-                screen.blit(self._color.value['FAST'], (piece.get_position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.get_position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
+                screen.blit(self._color.value['FAST'], (piece.position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
             elif piece.piece_type == PieceType.SMART:
-                screen.blit(self._color.value['SMART'], (piece.get_position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.get_position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
+                screen.blit(self._color.value['SMART'], (piece.position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
             elif piece.piece_type == PieceType.TOUGH:
-                screen.blit(self._color.value['TOUGH'], (piece.get_position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.get_position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
+                screen.blit(self._color.value['TOUGH'], (piece.position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
             if piece.is_down:
-                screen.blit(self._color.value['RIP'], (piece.get_position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.get_position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
+                screen.blit(self._color.value['RIP'], (piece.position[1]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2, piece.position[0]*GRIDWIDTH + (GRIDWIDTH-IMGSIZE)/2))
 
     def pick_strength(self):
         random.shuffle(self._strength_deck)
@@ -119,9 +105,6 @@ class Player:
         if len(self._strength_deck) == 0:
             self._strength_deck = [i for i in range(1, 6)]
         return picked
-
-
-    # setters?
 
 
 class HumanPlayer(Player):
@@ -141,12 +124,12 @@ class HumanPlayer(Player):
             init_pos = BLUE_POS
         i = 0
         for piece in self.pieces:
-            piece.set_position(init_pos[i], False, False)
+            piece.position = init_pos[i]
             i += 1
 
     def __str__(self):
         ans = "This is " + self._name + "'s Team : "
-        for piece in self._pieces:
+        for piece in self.pieces:
             ans += "\n" + str(piece)
         return ans
 
@@ -176,211 +159,14 @@ class AIPlayer(Player):
 
     def __str__(self):
         ans = "This is a" + str(self.level) + "AI's Team : "
-        for piece in self._pieces:
+        for piece in self.pieces:
             ans += "\n" + str(piece)
         return ans
 
 
-# class Game:
-#     """
-#     The state of the game, defined by:
-#     - a board;
-#     - a list of players
-#     - an integer used as the index of the next player in the list of players.
-#
-#     When building the list of players, the argument `player` is used and:
-#     - if an item is a string (`str`) then a human player is built using the
-#       string as their name;
-#     - if an item is a level (`Level`) then an AI player is built using the
-#       level.
-#     """
-#
-#     def __init__(self, players, pieces, colors, ball_position):
-#         self._players = []
-#         k = -1
-#         for player in players:
-#             k += 1
-#             if isinstance(player, str):
-#                 self._players.append(HumanPlayer(player, k, pieces[k], colors[k]))
-#             elif isinstance(player, Level):
-#                 self._players.append(AIPlayer(player, k, pieces[k], colors[k]))
-#             else:
-#                 assert False, "unknown player definition"
-#         self._currentPlayer = 0
-#         self._ball_position = ball_position
-#         self._board = self.board_state()
-#
-#     def __str__(self):
-#         ans = "Description of the first team :"
-#         ans += "\n" + str(self._players[0])
-#         ans += "\n" + " "
-#         ans += "\n" + "Description of the second team :"
-#         ans += "\n" + str(self._players[1])
-#         return ans
-#
-#     def get_positions(self):
-#         return[self._players[0].get_positions(), self._players[1].get_positions()]
-#
-#     def board_state(self):
-#         piece0 = Piece(-1, -1, PieceType.NONE, [-1, -1], False, False)
-#         board = []
-#         team1 = self._players[0].pieces
-#         team2 = self._players[1].pieces
-#         for i in range(i_limit):
-#             board.append([])
-#             for j in range(j_limit):
-#                 found_piece = False
-#                 for piece in team1:
-#                     if piece.get_position() == [i, j]:
-#                         board[i].append(piece)
-#                         found_piece = True
-#                 for piece in team2:
-#                     if piece.get_position() == [i, j]:
-#                         board[i].append(piece)
-#                         found_piece = True
-#                 if not found_piece:
-#                     board[i].append(piece0)
-#         return board
-#
-#     def update_positions(self, playerID: int, pieceID: int, new_position: [int, int], ball: bool, down: bool):
-#         self._players[playerID].update_positions(pieceID, new_position, ball, down)
-#
-#     def update_ball_position(self, new_position: [int, int]):
-#         self._ball_position = new_position
-#
-#     def generate_displacement(self, playerID: int, pieceID: int):
-#         moves = []
-#         piece = self._players[playerID].pieces[pieceID]
-#         for i in range(i_limit):
-#             for j in range(j_limit):
-#                 if 0 < abs(i-piece.get_position()[0])+abs(j-piece.get_position()[1]) <= piece.speed:
-#                     moves.append(Displacement(piece, [i, j]))
-#         return moves
-#
-#     def generate_ball_throwing(self, playerID: int, pieceID: int):
-#         pass
-#
-#     def generate_tackle(self, playerID: int, pieceID: int):
-#         pass
-#
-#     def generate_try(self, playerID: int, pieceID: int):
-#         pass
-#
-#     def update_board(self):
-#         self._board = self.board_state()
-#
-#     def face_off(self, attack_player_ID, attack_piece_ID, defense_piece_ID):
-#         attack_player = self._players[attack_player_ID]
-#         defense_player = self._players[(attack_player_ID + 1) % 2]
-#         attack_piece = attack_player.pieces[attack_piece_ID]
-#         defense_piece = defense_player.pieces[defense_piece_ID]
-#         attack_score = attack_player.pick_strength() + attack_piece.attack
-#         defense_score = defense_player.pick_strength() + defense_piece.defense
-#         if attack_score > defense_score :
-#             return "Attack wins!"
-#         if defense_score > attack_score :
-#             return "Defense wins!"
-#         else:
-#             attack_pick = attack_player.pick_strength()
-#             defense_pick = defense_player.pick_strength()
-#             if attack_pick > defense_pick :
-#                 return "Attack wins!"
-#             else:
-#                 return "Defense wins!"
-#
-#     def winners(self):
-#         pass
-
-
-class Move:
-    """
-    The parent class of all possible moves.
-    """
-
+class Board:
     def __init__(self):
-        pass
-
-class Displacement(Move):
-    """
-    The move of drawing cards from the train card deck.
-    """
-    def __init__(self, piece: Piece, new_position: Tuple[int, int], face_off_opponent=-1):
-        super().__init__()
-        self._piece = piece
-        self._new_position = new_position
-        self._face_off_opponent = face_off_opponent
-
-    def __str__(self):
-        return "Displacement of the piece " + str(self._piece.name) + " to the position : " + str(self._new_position)
-
-    # def play(self, game: Game):
-    #     if self._face_off_opponent == -1:
-    #         game.update_positions(self._piece.playerID, self._piece.ID, self._new_position, self._piece.ball(), False)
-    #         game.update_ball_position(self._new_position)
-    #     else:
-    #         result_face_off = game.face_off(self._piece.playerID, self._piece.ID, self._face_off_opponent)
-    #         if result_face_off == "Denfense wins!":
-    #             position = self._piece.get_position()
-    #             if self._piece.ball():
-    #                 game.update_ball_position([position[0], position[1] - 1])
-    #             game.update_positions(self._piece.playerID, self._piece.ID, position, False, True)
-    #         else:
-    #             game.update_positions(self._piece.playerID, self._piece.ID, self._new_position, self._piece.ball(), False)
-    #             game.update_ball_position(self._new_position)
-    #             game.update_positions((self._piece.playerID+1) % 2, self._face_off_opponent, False, True)
-
-
-class Pass(Move):
-    """
-    The move of passing the ball to another piece.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    def __str__(self):
-        return "pass"
-
-
-class Tackle(Move):
-    """
-    The move of Forcing a piece's way through, defined by the two pieces
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    def __str__(self):
-        return "Tackle"
-
-
-class FootKick(Move):
-    """
-        The move of Forcing a piece's way through, defined by the two pieces
-        """
-
-    def __init__(self):
-        super().__init__()
-
-    def __str__(self):
-        return "FootKick"
-
-
-class Try(Move):
-    """
-    The move of Trying,
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    def __str__(self):
-        return "Try"
-
-
-class Board():
-    def __init__(self):
-        self._board = [[None for _ in range(COLS)] for _ in range(ROWS)]
+        self.matrix = [[None for _ in range(COLS)] for _ in range(ROWS)]
         self._selected_piece: None
 
     def draw_bgd(self, screen):
@@ -395,7 +181,8 @@ class Board():
             player.draw(screen)
 
     def update_board(self, players):
+        self.matrix = [[None for _ in range(COLS)] for _ in range(ROWS)]
         for player in players:
             for piece in player.pieces:
-                self._board[piece.get_position[0]][piece.get_position[1]] = piece
+                self.matrix[piece.position[0]][piece.position[1]] = piece
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from kahmate import model
 from kahmate.settings import *
+from typing import Tuple
+
+
+PIECE0 = model.Piece(model.PieceType.BIG)
+PIECE0.position = [-1, -1]
 
 
 class Game:
@@ -17,22 +22,65 @@ class Game:
         pg.display.set_caption(TITLE)
 
         # define players
-        self._players = []
+        self.players = []
         for player, color in players:
             if isinstance(player, str):
-                self._players.append(model.HumanPlayer(player, color))
+                self.players.append(model.HumanPlayer(player, color))
             elif isinstance(player, model.Level):
-                self._players.append(model.AIPlayer(player, color))
+                self.players.append(model.AIPlayer(player, color))
             else:
                 assert False, "unknown player definition"
         self._next_player = 0
+        self._ball_position = [0, 0]
 
     def next_player(self):
-        return self._players[self._next_player]
+        return self.players[self._next_player]
 
     def update(self):
-        self._board.draw(self._screen, self._players)
+        self._board.draw(self._screen, self.players)
         pg.display.update()
+
+    def __str__(self):
+        ans = "Description of the first team :"
+        ans += "\n" + str(self.players[0])
+        ans += "\n" + " "
+        ans += "\n" + "Description of the second team :"
+        ans += "\n" + str(self.players[1])
+        return ans
+
+    def generate_displacement(self, piece: model.Piece):
+        # LOOPS TO BE OPTIMIZED
+        moves = []
+        for i in range(ROWS):
+            for j in range(COLS):
+                distance_ok = 0 < abs(i-piece.position[0]) + abs(j-piece.position[1]) <= piece.speed
+                empty_case = self._board.matrix[i][j] is None
+                if distance_ok and empty_case:
+                    moves.append(Displacement(piece, [i, j]))
+        return moves
+
+    def face_off(self, attack_piece: model.Piece, defense_piece : model.Piece):
+        attack_player = self.players[self._next_player]
+        defense_player = self.players[(self._next_player + 1) % 2]
+        attack_score = attack_player.pick_strength() + attack_piece.attack
+        defense_score = defense_player.pick_strength() + defense_piece.defense
+        if attack_score > defense_score :
+            return "Attack wins!"
+        if defense_score > attack_score :
+            return "Defense wins!"
+        else:
+            attack_pick = attack_player.pick_strength()
+            defense_pick = defense_player.pick_strength()
+            if attack_pick > defense_pick:
+                return "Attack wins!"
+            else:
+                return "Defense wins!"
+
+    def update_ball_position(self, new_position: [int, int]):
+        self._ball_position = new_position
+
+    def update_board(self):
+        self._board.update_board(self.players)
 
     def run(self):
         while self._running:
@@ -44,11 +92,110 @@ class Game:
             self.update()
 
 
+class Move:
+    """
+    The parent class of all possible moves.
+    """
+
+    def __init__(self):
+        pass
+
+
+class Displacement(Move):
+    """
+    The move of drawing cards from the train card deck.
+    """
+    def __init__(self, piece: model.Piece, new_position: [int, int], face_off_opponent=PIECE0):
+        super().__init__()
+        self._piece = piece
+        self._new_position = new_position
+        self._face_off_opponent = face_off_opponent
+
+    def __str__(self):
+        return "Displacement of the piece " + str(self._piece.name) + " to the position : " + str(self._new_position)
+
+    def play(self, game: Game):
+        if self._face_off_opponent.position == [-1, -1]:
+            self._piece.position = self._new_position
+            if self._piece.has_ball:
+                game.update_ball_position(self._new_position)
+        else:
+            result_face_off = game.face_off(self._piece, self._face_off_opponent)
+            if result_face_off == "Denfense wins!":
+                position = self._piece.position()
+                if self._piece.has_ball:
+                    game.update_ball_position([position[0], position[1] - 1])
+                self._piece.is_down = True
+            else:
+                self._piece.position = self._new_position,
+                if self._piece.has_ball:
+                    game.update_ball_position(self._new_position)
+                self._face_off_opponent.is_down = True
+            game.update_board()
+
+
+class Pass(Move):
+    """
+    The move of passing the ball to another piece.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "pass"
+
+
+class Tackle(Move):
+    """
+    The move of Forcing a piece's way through, defined by the two pieces
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "Tackle"
+
+
+class FootKick(Move):
+    """
+        The move of Forcing a piece's way through, defined by the two pieces
+        """
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "FootKick"
+
+
+class Try(Move):
+    """
+    The move of Trying,
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "Try"
+
+
 if __name__ == "__main__":
     first_player = input('Name of the first player:')
     second_player = input('Name of the second player:')
 
     game = Game([(first_player, model.PlayerColor.BLUE), (second_player, model.PlayerColor.PINK)])
+    game.update_board()
+    possible_displacements = game.generate_displacement(game.players[1].pieces[0])
+    displacement = possible_displacements[2]
+    displacement.play(game)
+
+    print("LIST OF DISPLACEMENTS")
+    for d in possible_displacements:
+        print(d)
+
     game.run()
 
 
